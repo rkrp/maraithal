c0dc16ff8dd73b5941756a1e18928043cb02baee
Krishna Ram Prakash <krp@gtux.in>
2015-01-20 18:10:00 +0530

    Rewritten the entire codebase. Added support for using a key to hide the text in particular pattern.

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Implementation of basic LSB stegnography on PNG images
 Works only on RGBA images.
 
 ##To Do:
-* LSB modification pattern according to given key, instead of the constant
-  pattern being followed.
-* Support for encrypting and hiding the cipher in the image
-* Add support for non-PNG images
+- [x] LSB modification pattern according to given key, instead of the constant pattern being followed.
+- [ ] Message decodng from key
+- [ ] Binary input support
+- [ ] Support for encrypting and hiding the cipher in the image
+- [ ] Add support for non-PNG images
 
--- lsb_stegno.py
+++ lsb_stegno.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image
+import random
+from itertools import izip
 
 class lsb_stegno:
+    #Channel to encode or decode
+    C_RED, C_GREEN, C_BLUE, C_ALPHA = range(4)
+
     def __init__(self, image_path):
         self.im = Image.open(image_path)
         self.width, self.height = self.im.size
@@ -12,38 +17,63 @@ class lsb_stegno:
 
     def text2binary(self, text):
         return ''.join(map(lambda x:bin(ord(x))[2:].zfill(8), text))
+    
+    """
+    Linear to row/col address translator
+    Returns a tuple with row/col address
+    """
+    def lin2rowcol(self, lin):
+        row = lin / self.width
+        col = lin - (row * self.width)
+        return (col, row)
+    
+    """
+    PRG is NOT cryptographically secure!!
+    You have been warned!!
+    """
+    def shuffle_k(self, key):
+        length = self.height * self.width
+        lin_pos = range(length)
 
-    def text_encode(self, text):
+        random.seed(key)
+        random.shuffle(lin_pos)
+        return lin_pos
+    
+    """
+    Encoding of text takes place here. This is the method which is directly
+    invoked from the application
+    """
+    def text_encode(self, text, key, mode=C_RED):
+        #Add \x00 to know the termination pos of the string
+        text += '\x00'
+
         payload = self.text2binary(text)
-        payload_length = len(payload)
+        p_len = len(payload)
 
         #Checking for length requirement		
-        if payload_length > (self.width * self.height):
+        if p_len > (self.width * self.height):
             raise Exception ("Error: Insufficient Size - Choose a larger image")
 
-        #Iterating through the rows
-        rp = 0	#Row Pointer
-        cp = 0	#Col Pointer
-        i = 0
-
-        for i in payload:
-            original = self.im.getpixel((cp,rp))
+        pos = self.shuffle_k(key)
+        for bit, l_addr in izip(payload, pos):
+            #Translate linear pos to row/col address
+            addr = self.lin2rowcol(l_addr)
+            bit = int(bit)
+            
+            pixel = list(self.im.getpixel(addr))
+ 
             #Now modifying the LSB
-            mod = bin(original[0])[:-1] + i
-            self.new_im[cp, rp] = (int(mod, 2), original[1], original[2], original[3])
-
-            #Moving the row and col pointers
-            if(cp == self.width - 1):
-                cp = 0
-                rp += 1
+            if bit:
+                pixel[mode] |= 1
             else:
-                cp += 1
-    
+                pixel[mode] &= ~1
+
+            self.new_im[addr] = tuple(pixel)
         self.im.save("new_image.png")
 
 def main():
     lsb = lsb_stegno("image.png")
-    lsb.text_encode("SomeText")
+    lsb.text_encode("someplaintext", "somekey")
 
     return 0
 
